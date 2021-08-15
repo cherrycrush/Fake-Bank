d9c46d82b29f1b45e36591ef4628bb7d4a1cd392
cherrycrush <phobosnz@gmail.com>
2021-08-16 11:49:14 +1200

    Setup styling for nav component

---
+++ client/components/Nav.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Nav () {
+  return (
+    <nav>
+      <ul className='nav-list'>
+        <li className='logo'>
+          KiwiBlank.
+        </li>
+        <li className='nav-item'>
+          Sign In
+        </li>
+        <li className='nav-item'>
+          Register
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
